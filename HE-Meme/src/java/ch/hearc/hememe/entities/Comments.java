adfc9b14501747de37b8c8c27fa04cb11308cb61
Sylvain Renaud <sylvain.renaud@he-arc.ch>
2018-03-08 13:19:52 +0100

    Add dumps, entities, Controller, facades, JSP.

---
+++ HE-Meme/src/java/ch/hearc/hememe/entities/Comments.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.hememe.entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author sylvain.renaud
+ */
+@Entity
+@Table(name = "comments")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Comments.findAll", query = "SELECT c FROM Comments c")
+    , @NamedQuery(name = "Comments.findById", query = "SELECT c FROM Comments c WHERE c.id = :id")
+    , @NamedQuery(name = "Comments.findByContent", query = "SELECT c FROM Comments c WHERE c.content = :content")
+    , @NamedQuery(name = "Comments.findByNbLike", query = "SELECT c FROM Comments c WHERE c.nbLike = :nbLike")})
+public class Comments implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 4096)
+    @Column(name = "content")
+    private String content;
+    @Column(name = "nb_like")
+    private Integer nbLike;
+    @JoinColumn(name = "post_id", referencedColumnName = "id")
+    @OneToOne(optional = false)
+    private Posts postId;
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @OneToOne(optional = false)
+    private User userId;
+
+    public Comments() {
+    }
+
+    public Comments(Integer id) {
+        this.id = id;
+    }
+
+    public Comments(Integer id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getNbLike() {
+        return nbLike;
+    }
+
+    public void setNbLike(Integer nbLike) {
+        this.nbLike = nbLike;
+    }
+
+    public Posts getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Posts postId) {
+        this.postId = postId;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Comments)) {
+            return false;
+        }
+        Comments other = (Comments) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ch.hearc.hememe.entities.Comments[ id=" + id + " ]";
+    }
+    
+}
---
+++ HE-Meme/src/java/ch/hearc/hememe/entities/Posts.java
@@ -0,0 +1,177 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.hememe.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author sylvain.renaud
+ */
+@Entity
+@Table(name = "posts")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Posts.findAll", query = "SELECT p FROM Posts p")
+    , @NamedQuery(name = "Posts.findById", query = "SELECT p FROM Posts p WHERE p.id = :id")
+    , @NamedQuery(name = "Posts.findByImageName", query = "SELECT p FROM Posts p WHERE p.imageName = :imageName")
+    , @NamedQuery(name = "Posts.findByTitle", query = "SELECT p FROM Posts p WHERE p.title = :title")
+    , @NamedQuery(name = "Posts.findByNbLike", query = "SELECT p FROM Posts p WHERE p.nbLike = :nbLike")
+    , @NamedQuery(name = "Posts.findByDatePost", query = "SELECT p FROM Posts p WHERE p.datePost = :datePost")})
+public class Posts implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "image_name")
+    private String imageName;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "title")
+    private String title;
+    @Column(name = "nb_like")
+    private Integer nbLike;
+    @Column(name = "date_post")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date datePost;
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "postId")
+    private Comments comments;
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Category categoryId;
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @OneToOne(optional = false)
+    private User userId;
+
+    public Posts() {
+    }
+
+    public Posts(Integer id) {
+        this.id = id;
+    }
+
+    public Posts(Integer id, String imageName, String title) {
+        this.id = id;
+        this.imageName = imageName;
+        this.title = title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getNbLike() {
+        return nbLike;
+    }
+
+    public void setNbLike(Integer nbLike) {
+        this.nbLike = nbLike;
+    }
+
+    public Date getDatePost() {
+        return datePost;
+    }
+
+    public void setDatePost(Date datePost) {
+        this.datePost = datePost;
+    }
+
+    public Comments getComments() {
+        return comments;
+    }
+
+    public void setComments(Comments comments) {
+        this.comments = comments;
+    }
+
+    public Category getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Category categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Posts)) {
+            return false;
+        }
+        Posts other = (Posts) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ch.hearc.hememe.entities.Posts[ id=" + id + " ]";
+    }
+    
+}
